ef4c86b808e20e42c8d8f205092bfff951b4b6e2
aizhuxue007 <aizhuprogram@gmail.com>
2023-09-24 22:02:42 -0400

    a

---
+++ reactgraph-prc/src/App.jsx
@@ -0,0 +1,38 @@
+const App = () => {
+  const data = [
+    {
+      day: "Sunday",
+      pomodoros: 1,
+    },
+    {
+      day: "Monday",
+      pomodoros: 2,
+    },
+    {
+      day: "Tuesday",
+      pomodoros: 4,
+    },
+    {
+      day: "Wednesday",
+      pomodoros: 9,
+    },
+    {
+      day: "Thursday",
+      pomodoros: 5,
+    },
+    {
+      day: "Friday",
+      pomodoros: 7,
+    },
+    {
+      day: "Saturday",
+      pomodoros: 20,
+    },
+  ];
+
+  return (
+    <></>
+  );
+};
+
+export default App;
